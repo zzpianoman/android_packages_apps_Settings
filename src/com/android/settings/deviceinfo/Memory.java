e4d45783c60b1de1961b14b64951d499d2c40c7c
Lars Greiss <kufikugel@googlemail.com>
2014-11-30 21:20:57 +0100

    Settings: Media Scanner on boot behaviour (2/3)
    Settings part - fb part is merged and mediaprovider repo was patched up to LP
    
    Change-Id: Ia7ef8a52e75b5c111eeddbbf04df22ed9cc8d847

--- src/com/android/settings/deviceinfo/Memory.java
+++ src/com/android/settings/deviceinfo/Memory.java
@@ -34,13 +34,18 @@ import android.os.Environment;
 import android.os.IBinder;
 import android.os.RemoteException;
 import android.os.ServiceManager;
+import android.os.UserHandle;
 import android.os.UserManager;
 import android.os.storage.IMountService;
 import android.os.storage.StorageEventListener;
 import android.os.storage.StorageManager;
 import android.os.storage.StorageVolume;
+import android.preference.ListPreference;
 import android.preference.Preference;
+import android.preference.PreferenceCategory;
 import android.preference.PreferenceScreen;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -63,10 +68,14 @@ import java.util.List;
  * Panel showing storage usage on disk for known {@link StorageVolume} returned
  * by {@link StorageManager}. Calculates and displays usage of data types.
  */
-public class Memory extends SettingsPreferenceFragment implements Indexable {
+public class Memory extends SettingsPreferenceFragment
+        implements Indexable, OnPreferenceChangeListener {
+
     private static final String TAG = "MemorySettings";
 
     private static final String TAG_CONFIRM_CLEAR_CACHE = "confirmClearCache";
+    private static final String PREF_OPTIONS_CATEGORY = "settings_options_prefs";
+    private static final String PREF_MEDIA_SCANNER_ON_BOOT = "media_scanner_on_boot";
 
     private static final int DLG_CONFIRM_UNMOUNT = 1;
     private static final int DLG_ERROR_UNMOUNT = 2;
@@ -76,6 +85,8 @@ public class Memory extends SettingsPreferenceFragment implements Indexable {
     // one's preference is disabled
     private static Preference sLastClickedMountToggle;
     private static String sClickedMountPoint;
+
+    private ListPreference mMsob;
 
     // Access using getMountService()
     private IMountService mMountService;
@@ -108,6 +119,19 @@ public class Memory extends SettingsPreferenceFragment implements Indexable {
             if (volume.isRemovable()) {
                 mRemoveableStorage = true;
             }
+        }
+
+        mMsob = (ListPreference) findPreference(PREF_MEDIA_SCANNER_ON_BOOT);
+        if (UserHandle.myUserId() != UserHandle.USER_OWNER) {
+            PreferenceCategory options =
+                    (PreferenceCategory) findPreference(PREF_OPTIONS_CATEGORY);
+            getPreferenceScreen().removePreference(options);
+        } else {
+            mMsob.setValue(String.valueOf(
+                    Settings.System.getInt(getActivity().getContentResolver(),
+                    Settings.System.MEDIA_SCANNER_ON_BOOT, 0)));
+            mMsob.setSummary(mMsob.getEntry());
+            mMsob.setOnPreferenceChangeListener(this);
         }
 
         setHasOptionsMenu(true);
@@ -267,6 +291,21 @@ public class Memory extends SettingsPreferenceFragment implements Indexable {
             }
         }
 
+        return false;
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        String value = (String) newValue;
+        if (preference == mMsob) {
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.MEDIA_SCANNER_ON_BOOT,
+                    Integer.valueOf(value));
+
+            mMsob.setValue(String.valueOf(value));
+            mMsob.setSummary(mMsob.getEntry());
+            return true;
+        }
         return false;
     }
 
