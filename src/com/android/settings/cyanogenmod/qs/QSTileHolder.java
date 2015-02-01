466d13e6f4985c8270abb239f7ba77e6ccb3d271
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-02-01 07:23:50 +0000

    QS: add Screenshot tile (2/2)
    Change-Id: Ieb3eddd45be2a8c6c4bf19b5931c40998794324d

--- src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
+++ src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
@@ -161,6 +161,10 @@ public class QSTileHolder {
                 resourceName = "ic_qs_sync_on";
                 stringId = R.string.qs_tile_sync;
                 break;
+            case QSConstants.TILE_SCREENSHOT:
+                resourceName = "ic_qs_screenshot";
+                stringId = R.string.qs_screenshot_tile;
+                break;
             default:
                 return null;
         }
