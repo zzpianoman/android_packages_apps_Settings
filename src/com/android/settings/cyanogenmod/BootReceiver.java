dd8ba415c1c1bb0629611eeda0addb7314d9ffb5
Steve Kondik <steve@cyngn.com>
2015-02-18 03:44:31 -0800

    livedisplay: Add LiveDisplay settings
     * Add preference for LiveDisplay mode, and additional supporting
       options for auto mode.
     * CABC, SRE, CE are all under this umbrella now.
     * Manual RGB tweaking is now handled here and the requirement for
       CMHW has been removed as it will go thru RenderEngine.
     * Moved all related code into the livedisplay package.
    
    Conflicts:
    	AndroidManifest.xml
    	src/com/android/settings/DisplaySettings.java
    	src/com/android/settings/SettingsActivity.java
    
    Change-Id: I0b00f2bfd8f8227d64ad271bbe0f004f0b4bcf9d
    
    	modified:   AndroidManifest.xml
    	new file:   res/drawable/color_temperature_preview.xml
    	modified:   res/layout/display_color_calibration.xml
    	new file:   res/layout/display_temperature.xml
    	modified:   res/values/cm_strings.xml
    	modified:   res/xml/display_settings.xml
    	new file:   res/xml/livedisplay.xml
    	modified:   src/com/android/settings/DisplaySettings.java
    	modified:   src/com/android/settings/Settings.java
    	modified:   src/com/android/settings/SettingsActivity.java
    	modified:   src/com/android/settings/Utils.java
    	modified:   src/com/android/settings/cyanogenmod/BootReceiver.java
    	renamed:    src/com/android/settings/hardware/DisplayColor.java -> src/com/android/settings/livedisplay/DisplayColor.java
    	renamed:    src/com/android/settings/hardware/DisplayGamma.java -> src/com/android/settings/livedisplay/DisplayGamma.java
    	new file:   src/com/android/settings/livedisplay/DisplayTemperature.java
    	new file:   src/com/android/settings/livedisplay/LiveDisplay.java

--- src/com/android/settings/cyanogenmod/BootReceiver.java
+++ src/com/android/settings/cyanogenmod/BootReceiver.java
@@ -19,10 +19,10 @@ package com.android.settings.cyanogenmod;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import com.android.settings.DisplaySettings;
-import com.android.settings.hardware.DisplayColor;
-import com.android.settings.hardware.DisplayGamma;
+import com.android.settings.R;
+import com.android.settings.Utils;
 import com.android.settings.hardware.VibratorIntensity;
+import com.android.settings.livedisplay.DisplayGamma;
 
 public class BootReceiver extends BroadcastReceiver {
 
@@ -31,9 +31,7 @@ public class BootReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context ctx, Intent intent) {
         /* Restore the hardware tunable values */
-        DisplayColor.restore(ctx);
         DisplayGamma.restore(ctx);
         VibratorIntensity.restore(ctx);
-        DisplaySettings.restore(ctx);
     }
 }
