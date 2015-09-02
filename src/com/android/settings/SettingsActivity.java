SlimRoms/packages_apps_Settings

QS: expanded desktop tile [2/2]
Change-Id: I1db8bfe71f6293cbd164f9e258a93415407a61f5
PS: adapted to the expanded desktop entry moved into the Interface Settings

src/com/android/settings/SettingsActivity.java
@@ -68,6 +68,7 @@ import com.android.settings.accessibility.AccessibilitySettings;
 import com.android.settings.accessibility.CaptionPropertiesFragment;
 import com.android.settings.accounts.AccountSettings;
 import com.android.settings.accounts.AccountSyncSettings;
+import com.android.settings.applications.ExpandedDesktopPreferenceFragment;
 import com.android.settings.applications.InstalledAppDetails;
 import com.android.settings.applications.ManageApplications;
 import com.android.settings.applications.ProcessStatsUi;
@@ -304,7 +305,8 @@ public class SettingsActivity extends Activity
             AppNotificationSettings.class.getName(),
             OtherSoundSettings.class.getName(),
             QuickLaunchSettings.class.getName(),
-            ApnSettings.class.getName()
+            ApnSettings.class.getName(),
+            ExpandedDesktopPreferenceFragment.class.getName()
     };
 
 

zzpianoman/android_packages_apps_Settings

src/com/android/settings/SettingsActivity.java
@@ -68,6 +68,7 @@ import com.android.settings.accessibility.AccessibilitySettings;
 import com.android.settings.accessibility.CaptionPropertiesFragment;
 import com.android.settings.accounts.AccountSettings;
 import com.android.settings.accounts.AccountSyncSettings;
+import com.android.settings.applications.ExpandedDesktopPreferenceFragment;
 import com.android.settings.applications.InstalledAppDetails;
 import com.android.settings.applications.ManageApplications;
 import com.android.settings.applications.ProcessStatsUi;
@@ -324,7 +325,8 @@ public class SettingsActivity extends Activity
             BarsSettings.class.getName(),
             ButtonBrightnessSettings.class.getName(),
             com.android.settings.cyanogenmod.PrivacySettings.class.getName(),
-            QsSettings.class.getName()
+            QsSettings.class.getName(),
+            ExpandedDesktopPreferenceFragment.class.getName()
     };
 
 